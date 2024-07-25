7a6c2a2be24e2c580d6b65bb4b47eae0afaa461b
RandyGaul <randypgaul@gmail.com>
2024-07-25 06:24:29 -0700

    Committed wrong glsl file

--- shaders/gray.glsl
+++ shaders/gray.glsl
@@ -9,6 +9,10 @@ uniform sampler2D tex;
 
 vec4 shader(vec4 color, vec2 pos, vec2 atlas_uv, vec2 screen_uv, vec4 params)
 {
+	float alpha = texture(tex, screen_uv).a;
+    if (alpha == 0.0) {
+        discard;
+    }
 	return vec4(0.1, 0.1, 0.1, alpha*0.5);
 }
 @end
