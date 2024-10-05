009463a4c6da1ccb73ee9a3bef8ab16134bd6cba
Charles Waldner <waldner.charles@gmail.com>
2024-10-05 10:09:22 -0600

    Fix main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,23 +1,26 @@
+function update() end
+
 function main()
-	make_app("cute imgui image test", default_display(), 0, 0, 640, 480, APP_OPTIONS_WINDOW_POS_CENTERED_BIT | APP_OPTIONS_RESIZABLE_BIT, argv0)
-	app_init_imgui()
-	mount_directory_as("../content", "/")
-	
-	sprite = make_demo_sprite()
-	assert(sprite)
-	
-	while app_is_running() do
-		app_update()
-		draw_sprite(sprite)
-		
-		draw_text("Press space", 0,200, -1)
-		
-		imgui_begin("Room Editor", true)
-		imgui_button("click me")
-		check = imgui_checkbox("yo", check)
-		imgui_sprite_button("cf", sprite)
-		imgui_end()
-		
-		app_draw_onto_screen(true)
-	end
+    make_app("cute imgui image test", default_display(), 0, 0, 640, 480,
+             bit.bor(APP_OPTIONS_WINDOW_POS_CENTERED_BIT,
+                     APP_OPTIONS_RESIZABLE_BIT), argv0)
+    app_init_imgui()
+    mount_directory_as("../content", "/")
+
+    sprite = make_demo_sprite()
+    assert(sprite)
+    while app_is_running() do
+        app_update("update")
+        draw_sprite(sprite)
+
+        draw_text("Press space", 0, 200, -1)
+
+        imgui_begin("Room Editor", true)
+        imgui_button("click me")
+        check = imgui_checkbox("yo", check)
+        imgui_sprite_button("cf", sprite)
+        imgui_end()
+
+        app_draw_onto_screen(true)
+    end
 end
